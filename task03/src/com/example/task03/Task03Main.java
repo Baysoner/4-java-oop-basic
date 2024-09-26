00baed3fac8f763a25f37d161776c0233c97e778
Artem Solovyov <artemsolovev121@gmail.com>
2024-09-26 21:04:28 +0500

    Соловьев Артём. Задание 03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,13 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
+        ComplexNumber num1 = new ComplexNumber(2.0, 3.0);
+        ComplexNumber num2 = new ComplexNumber(4.0, -5.0);
 
+        ComplexNumber sum = num1.add(num2);
+        System.out.println("Сумма: " + sum);
+
+        ComplexNumber product = num1.multiply(num2);
+        System.out.println("Произведение: " + product);
     }
 }
